b39ef66411699bd42147cd46aee2affab09bb24d
yavuzim <kyavuzimer@gmail.com>
2023-11-16 17:38:50 +0300

    Siniflar olusturuldu.

---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Cariler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Cariler
+    {
+        [Key]
+        public int CariId { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
+        public string CariAd { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
+        public string CariSoyadi { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(13)]
+        public string CariSehir { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(50)]
+        public string CariMail { get; set; }
+        public SatisHareket SatisHareket { get; set; }
+    }
+}
