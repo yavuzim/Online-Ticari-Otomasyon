23f0744b835830ed43bc8303927b282298ca6a05
yavuzim <kyavuzimer@gmail.com>
2024-01-07 20:18:43 +0300

    faturalar

---
+++ MvcOnlineTicariOtomasyon/Controllers/FaturaController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class FaturaController : Controller
+    {
+        // GET: Fatura
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var liste = c.Faturalars.ToList();
+            return View(liste);
+        }
+        [HttpGet]
+        public ActionResult FaturaEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult FaturaEkle(Faturalar d)
+        {
+            c.Faturalars.Add(d);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
