97207e100bee474d703f735c1941b0613eff767d
yavuzim <kyavuzimer@gmail.com>
2024-01-10 17:15:58 +0300

    personeller cariler

--- MvcOnlineTicariOtomasyon/Controllers/FaturaController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/FaturaController.cs
@@ -27,5 +27,39 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             c.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult FaturaGetir(int id)
+        {
+            var fat = c.Faturalars.Find(id);
+            return View("FaturaGetir", fat);
+        }
+        public ActionResult FaturaGuncelle(Faturalar k)
+        {
+            var fat = c.Faturalars.Find(k.FaturaId);
+            fat.FaturaSeriNo = k.FaturaSeriNo;
+            fat.FaturaSiraNo = k.FaturaSiraNo;
+            fat.Saat = k.Saat;
+            fat.Tarih = k.Tarih;
+            fat.TeslimAlan = k.TeslimAlan;
+            fat.TeslimEden = k.TeslimEden;
+            fat.VergiDairesi = k.VergiDairesi;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult FaturaDetay(int id)
+        {
+            var degerler = c.FaturaKalems.Where(x => x.FaturaKalemId == id).ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniKalem()
+        {
+            return View();
+        }
+        public ActionResult YeniKalem(FaturaKalem p)
+        {
+            c.FaturaKalems.Add(p);
+            c.SaveChanges();
+            return View("Index");
+        }
     }
 }
--- MvcOnlineTicariOtomasyon/Models/Siniflar/FaturaKalem.cs
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/FaturaKalem.cs
@@ -18,6 +18,7 @@ namespace MvcOnlineTicariOtomasyon.Models.Siniflar
         public int Miktar { get; set; }
         public decimal BirimFiyat { get; set; }
         public decimal Tutar { get; set; }
+        public int Faturaid { get; set; }
         public Faturalar Faturalar { get; set; }
     }
 }
